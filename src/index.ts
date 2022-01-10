799c666d19f5f7392cb5e0d03172ab8ad5a2367f
Eric Wu <e-wu@users.noreply.github.com>
2022-01-10 06:28:21 -0500

    Add in exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
+// Common
 export { CommonRepositories } from "./Common/Common.repository";
 export { Constants } from "./Common/Constants";
 export { ProjectCacheService } from "./Common/ProjectCache.service";
 export { ProjectService } from "./Common/Project.service";
-export { SearchRepository } from "./Search/Search.repository";
-export { SearchResultEntity } from "./Search/SearchResult.entity";
 export { TreeNode } from "./Common/TreeNode";
 export { WorkItemBaseEntity } from "./Common/WorkItemBase.entity";
 export { WorkItemBaseWithPredecessor } from "./Common/WorkItemBaseWithPredecessor.entity";
+
+// Search
+export { SearchRepository } from "./Search/Search.repository";
+export { SearchResultEntity } from "./Search/SearchResult.entity";
+
+// Work Item Types
+export { IWorkItemTypeFactory } from "./WorkItemProcess/IWorkItemType.factory";
+export { WorkItemProcessService } from "./WorkItemProcess/WorkItemProcess.service";
+export { WorkItemTypeFactory } from "./WorkItemProcess/WorkItemType.factory";
+export { WorkItemTypeEntity } from "./WorkItemProcess/WorkItemType";
+
+// Function export
 export { to } from "./Common/PromiseWrap";
